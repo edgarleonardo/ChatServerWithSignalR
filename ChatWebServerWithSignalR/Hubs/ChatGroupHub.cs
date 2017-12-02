ee3ad8e43c898956a1f3f98e12818d00fdcc8972
edgarleonardo.perez <edgarleonardo.perez@outlook.com>
2017-12-02 07:37:44 -0400

    Add project files.

---
+++ ChatWebServerWithSignalR/Hubs/ChatGroupHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace ChatApplication.Hubs
+{
+    public class ChatGroupHub : Hub
+    {
+        public void SendMessage(string message, string group, string name)
+        {
+            Clients.Group(group).GetMessage(name, message);
+        }
+
+        public Task JoinGroup(string group)
+        {
+            return Groups.Add(Context.ConnectionId, group);
+        }
+
+        public Task LeaveGroup(string group)
+        {
+            return Groups.Remove(Context.ConnectionId, group);
+        }
+    }
+}
---
+++ ChatWebServerWithSignalR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatWebServerWithSignalR.Startup))]
+namespace ChatWebServerWithSignalR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
